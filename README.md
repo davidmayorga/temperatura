49b19187e2943195970525aec0a754533ad14a00
davidmayorga <davidmayorga@colegioguanenta.edu.co>
2022-04-21 17:55:46 -0500

    Creacion de diagrama de flujo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programa No. 3 Covertir grados celcius a kerlvin y farenheit
+
+![Diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ temperatura.py
@@ -0,0 +1,18 @@
+"Programa No. 3"
+"Programa para calcular grados kelvin y celsius"
+
+print ("----------------")
+print ("----Grados celsius a farengeit y a kelvin----")
+print ("----------------")
+
+#input
+
+c=int(input("Digite los grados"))
+
+#processing 
+k=c+273.15
+f=c*1.8+32
+
+#ouput
+print(str(c)+" grados celsius son "+str(k)+" grados kelvin y "+str(f)+" grados fahrenheit")
+
